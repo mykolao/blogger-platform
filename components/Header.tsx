e5f54dfbdde452e5e8f93325c6f715059bb72407
mykolao <mykolaoleskiv@gmail.com>
2022-11-26 01:10:25 +0200

    feat: Header component

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import styles from "styles/Header.module.scss";
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      <header>
+        <h1>Blogger Platform</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
