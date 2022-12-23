90fc364387c9a1402aff4572ab42802ef12dcc5a
mykolao <mykolaoleskiv@gmail.com>
2022-12-23 22:17:25 +0200

    style: set min-width

--- components/Navbar/Navbar.styled.tsx
+++ components/Navbar/Navbar.styled.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
 export const NavbarStyled = styled.aside`
-  max-width: ${({ theme }) => theme.navWidth};
+  min-width: ${({ theme }) => theme.navWidth};
   margin: 38px 0 0 8px;
 `;
