8dca5c3a55b94fdc95da4235964a7b70c503d756
mykolao <mykolaoleskiv@gmail.com>
2022-12-23 22:28:55 +0200

    init posts route

---
+++ pages/posts/index.tsx
@@ -0,0 +1,18 @@
+import Head from "next/head";
+
+import Layout from "components/Layout";
+
+const Posts = () => {
+  return (
+    <>
+      <Head>
+        <title>Posts | Blogger</title>
+      </Head>
+      <Layout>
+        <h2>Posts</h2>
+      </Layout>
+    </>
+  );
+};
+
+export default Posts;
