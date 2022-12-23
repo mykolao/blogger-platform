293e6c1a7f9d61faa0722870d02da900d953da82
mykolao <mykolaoleskiv@gmail.com>
2022-12-23 22:30:56 +0200

    provide styled context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+
+import StyledProvider from "styled/StyledProvider";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <StyledProvider>
+      <Component {...pageProps} />
+    </StyledProvider>
+  );
 }
