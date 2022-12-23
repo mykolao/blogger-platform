a6cc189a9b32af900708ca04a9c452ebfb890237
mykolao <mykolaoleskiv@gmail.com>
2022-12-23 22:04:05 +0200

    export types

---
+++ types/index.ts
@@ -0,0 +1,4 @@
+export type { ChildrenProp } from "types/ChildrenProp";
+export type { TBlog } from "types/TBlog";
+export type { TBlogList } from "types/TBlogList";
+export type { TPost } from "types/TPost";
