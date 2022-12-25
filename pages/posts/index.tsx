7c19bdb94adc40b58164f4656c632a8d11a48369
mykolao <mykolaoleskiv@gmail.com>
2022-12-26 00:09:50 +0200

    feat: posts page

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,15 +1,44 @@
 import Head from "next/head";
 
 import Layout from "components/Layout";
+import { TPostList } from "types";
+import { GetServerSideProps } from "next";
+import { postAPI } from "api";
+import styled from "styled-components";
+import PageTitle from "components/PageTitle";
+import Divider from "components/Divider";
+import PostList from "components/PostList";
 
-const Posts = () => {
+interface Props {
+  posts: TPostList;
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const res = await postAPI.getPosts();
+  const posts = res.items ?? [];
+
+  return {
+    props: { posts }
+  };
+};
+
+const Container = styled.div`
+  margin: 0 0 0 24px;
+`;
+
+const Posts: React.FC<Props> = ({ posts }) => {
   return (
     <>
       <Head>
         <title>Posts | Blogger</title>
       </Head>
       <Layout>
-        <h2>Posts</h2>
+        <Container>
+          <PageTitle title="Posts" />
+          <Divider />
+          {/* TODO: Sort */}
+          <PostList posts={posts} />
+        </Container>
       </Layout>
     </>
   );
