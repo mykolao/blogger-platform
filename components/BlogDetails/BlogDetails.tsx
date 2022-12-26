91445212c961cce72dcc912a35c364aba9fff4dc
mykolao <mykolaoleskiv@gmail.com>
2022-12-27 00:14:06 +0200

    feat: render PostList

--- components/BlogDetails/BlogDetails.tsx
+++ components/BlogDetails/BlogDetails.tsx
@@ -6,22 +6,27 @@ import { CoverImage } from "components/BlogDetails/CoverImage";
 import Description from "components/BlogDetails/Description";
 import placeholder from "public/images/placeholder.svg";
 import { TBlog } from "types/TBlog";
+import { TPostList } from "types";
+import PostList from "components/BlogDetails/PostList";
+import Divider from "components/Divider";
 
 interface Props {
   value: TBlog;
+  posts: TPostList;
 }
 
 const Container = styled.section`
   margin-top: 12px;
+  margin-right: 24px;
 `;
 
-export const BlogDetails: FC<Props> = ({ value }) => {
-  // const { name } = value;
-
+export const BlogDetails: FC<Props> = ({ value, posts }) => {
   return (
     <Container>
       <CoverImage src={placeholder} />
       <Description value={value} />
+      <Divider />
+      <PostList posts={posts} />
     </Container>
   );
 };
