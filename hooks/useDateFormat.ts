599fd97690addf992b7f037cc831982ec77ecff9
mykolao <mykolaoleskiv@gmail.com>
2022-12-26 23:23:28 +0200

    fix: day value

--- hooks/useDateFormat.ts
+++ hooks/useDateFormat.ts
@@ -2,7 +2,7 @@ export const useDateFormat = (value: string) => {
   const dateInstance = new Date(value);
 
   const month = dateInstance.getMonth();
-  const day = dateInstance.getDay();
+  const day = dateInstance.getDate();
   const year = dateInstance.getFullYear();
 
   const dateStr = [month, day, year].join(".");
